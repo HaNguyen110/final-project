6d6379306aa70b12335d552d12747b6fb6dc346d
HaNguyen110 <hanguyen091130@gmail.com>
2021-04-09 10:08:16 +0700

    Add new format for pages

---
+++ coffee-shop/src/pages/about/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+import SEO from "../../components/SEO";
+
+import Hero from "../../layouts/Hero";
+import InfoUs from "../../layouts/InfoUs";
+import OurMission from "../../layouts/OurMission";
+import Footer from "../../layouts/Footer";
+import OurPromise from "../../layouts/ourPromise";
+import Flickity from "../../layouts/Flickity";
+import OurMethods from "../../layouts/OurMethods";
+
+import {
+  infoUsData,
+  ourPromiseData,
+  ourMissionData,
+  flickityData,
+  ourMethodsData,
+} from "../../constants/aboutMocksData";
+
+type Props = {};
+
+const About = ({}: Props) => {
+  return (
+    <div>
+      <SEO title="About page" description="This is About page" />
+
+      <Hero src="/bgAbout.jpg" title="Về chúng tôi" />
+
+      <InfoUs infoUsData={infoUsData} />
+
+      <OurMission ourMissionData={ourMissionData} />
+
+      <OurPromise ourPromiseData={ourPromiseData} />
+
+      <Flickity flickityData={flickityData} />
+
+      <OurMethods ourMethodsData={ourMethodsData} />
+
+      <Footer />
+    </div>
+  );
+};
+
+export default About;
---
+++ coffee-shop/src/pages/menu/index.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import { menuData } from "../../constants/menuMocksData";
+
+import styles from "./Menu.module.css";
+
+import MenuCategory from "../../layouts/Categories";
+import Hero from "../../layouts/Hero";
+import Footer from "../../layouts/Footer";
+
+const Menu = () => {
+  return (
+    <>
+      <Hero src="/bgMenu.jpg" title="menu" />
+
+      <div className="container">
+        {menuData.map((item) => {
+          return (
+            <div className={`${styles.categories} row`}>
+              <MenuCategory menuData={item} key={item.id} />
+            </div>
+          );
+        })}
+      </div>
+
+      {/* <Footer /> */}
+    </>
+  );
+};
+
+export default Menu;
